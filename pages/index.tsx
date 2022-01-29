3c315159cb027017464a43431437f2318ec6f0ed
gavinmeier25 <gavinmeier41910@gmail.com>
2022-01-29 10:12:47 -0600

    adding open graph tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { References, ReferenceProps } from '../components/independent/References
 import { Services, IService } from '../components/independent/Services';
 import { getHomePageDataProps, getLayoutAndMetaDataProps, LayoutMetaProps } from '../helpers/getPropData';
 import { HeroData } from '../components/independent/Hero';
+import Head from 'next/head';
 
 interface HomePageProps {
   references: ReferenceProps[];
@@ -15,6 +16,13 @@ type Props = HomePageProps & LayoutMetaProps;
 const Home: NextPage<Props> = ({ references, services }) => {
   return (
     <>
+      <Head>
+        <meta property="og:title" content="Gavin Meier" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="https://www.gavinmeier.com/_next/image?url=%2Favatar.svg&w=1920&q=75" />
+        <meta property="og:url" content="https://gavinmeier.com" />
+        {/* <meta property="twitter:card" content="Gavin Meier" /> */}
+      </Head>
       <Services services={services} />
       <References references={references} />
     </>
